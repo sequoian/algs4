083497a8074255d7e8c59d737a7f8bc491cfe665
Zach <zach303@msn.com>
2018-01-10 12:17:58 -0800

    isGoal and twin

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -14,7 +14,7 @@ public class Board {
     }
     
     public int dimension() {
-        return tiles.length / 2;
+        return dim;
     }
     
     public int hamming() {
@@ -31,8 +31,7 @@ public class Board {
     
     public int manhattan() {
         int priority = 0;
-        int n = tiles.length;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < tiles.length; i++) {
             int tile = tiles[i];
             if (tile != 0 && tile != i+1) {
                 int offset = Math.abs((i+1) - tile);
@@ -44,11 +43,45 @@ public class Board {
     }
     
     public boolean isGoal() {
-        
+        boolean goal = true;
+        for (int i = 0; i < tiles.length-1; i++) {
+            if (tiles[i] != i+1) {
+                goal = false;
+                break;
+            }
+        }
+        return goal;
     }
     
     public Board twin() {
-        
+        // create clone
+        int[][] clone = new int[dim][dim];
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                clone[i][j] = tiles[i*dim + j];
+            }
+        }
+        // alter clone
+        int iSwap = -1;
+        int jSwap = -1;
+        outerloop:  // allows for breaking out of nested loop
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                if (clone[i][j] != 0) {
+                    if (iSwap > -1) {
+                        int swap = clone[i][j];
+                        clone[i][j] = clone[iSwap][jSwap];
+                        clone[iSwap][jSwap] = swap;
+                        break outerloop;
+                    }
+                    else {
+                        iSwap = i;
+                        jSwap = j;
+                    }
+                }
+            }
+        }
+        return new Board(clone); 
     }
     
     public boolean equals(Object y) {
