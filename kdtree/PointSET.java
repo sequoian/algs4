30ed006470cae768149448fe8a9e852fb15d7cf4
Zach <zach303@msn.com>
2018-01-18 10:04:28 -0800

    start of project 5

---
+++ kdtree/PointSET.java
@@ -0,0 +1,38 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+
+public class PointSET {
+    
+    public PointSET() {
+        
+    }
+    
+    public boolean isEmpty() {
+        
+    }
+    
+    public int size() {
+        
+    }
+    
+    public void insert(Point2D p) {
+        
+    }
+    
+    public boolean contains(Point2D p) {
+        
+    }
+    
+    public void draw() {
+        
+    }
+    
+    public Iterable<Point2D> range(RectHV rect) {
+        
+    }
+    
+    public Point2D nearest(Point2D p) {
+        
+    }
+}
